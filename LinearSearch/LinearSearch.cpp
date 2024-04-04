864b7aa0bda32abd4af431922e8508edbbdebb06
Putranugroho14 <putranugroho61@gmail.com>
2024-04-04 11:18:31 +0700

    membuat void input

--- LinearSearch/LinearSearch.cpp
+++ LinearSearch/LinearSearch.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 using namespace std;
 
-
 int arr[20]; // Array to be searched int n; int i;
 int n;       // Number of elements in the array
 int i;       // Index of array element
+
+void input()
 
 
 int main()
