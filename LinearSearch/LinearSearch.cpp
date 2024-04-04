586490cceb663629797d15740e2a6eadca05c48c
Putranugroho14 <putranugroho61@gmail.com>
2024-04-04 11:22:44 +0700

    membuat if dan else

--- LinearSearch/LinearSearch.cpp
+++ LinearSearch/LinearSearch.cpp
@@ -11,7 +11,12 @@ void input()
 	{
 		cout << "Enter the number of elements in the array: ";
 		cin >> n;
-		
+		if ((n > 0) && (n <= 20))
+			break;
+		else
+			cout << "InArray should have minimum 1 and maximum 20 elements. \n\n";
+
+	}
 
 
 int main()
